36a981397675270d4d0957bf40d6a5b665a10689
Aliaksei Suslau <aliaksei.suslau02@gmail.com>
2023-10-20 18:46:16 +0200

    Persistent actors

---
+++ akka-persistence/src/main/scala/part2eventsourcing/PersistentActors.scala
@@ -0,0 +1,47 @@
+package part2eventsourcing
+
+import akka.actor.{ActorLogging, ActorSystem, Props}
+import akka.persistence.PersistentActor
+
+import java.util.Date
+
+object PersistentActors extends App {
+  // command
+  case class Invoice(recipient: String, date: Date, amount: Int)
+
+  // event
+  case class InvoiceRecorded(id: Int, recipient: String, date: Date, amount: Int)
+
+  class Accountant extends PersistentActor with ActorLogging {
+
+    var latestInvoiceId = 0
+    var totalAmount     = 0
+
+    override def persistenceId: String = "simple-accountant"
+
+    override def receiveCommand: Receive = {
+      case Invoice(recipient, date, amount) =>
+        log.info(s"Receive invoice for amount: $amount")
+        val event = InvoiceRecorded(latestInvoiceId, recipient, date, amount)
+        persist(event) { e =>
+          latestInvoiceId += 1
+          totalAmount += amount
+          log.info(s"Persisted $e as invoice #${e.id}, for total amount $totalAmount")
+        }
+    }
+
+    override def receiveRecover: Receive = {
+      case InvoiceRecorded(id, _, _, amount) =>
+        latestInvoiceId = id
+        totalAmount += amount
+        log.info(s"Recovered invoice #$id for amount $amount, total amount: $totalAmount")
+    }
+  }
+
+  val system     = ActorSystem("PersistentActors")
+  val accountant = system.actorOf(Props[Accountant], "simpleAccountant")
+
+  for (i <- 1 to 10) {
+    accountant ! Invoice("Company", new Date, i * 1000)
+  }
+}
