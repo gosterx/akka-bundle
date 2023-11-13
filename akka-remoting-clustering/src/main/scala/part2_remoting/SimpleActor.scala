d8cd4c37146750a6fe05daeb68769d90196d81d8
Aliaksei Suslau <aliaksei.suslau02@gmail.com>
2023-11-13 22:28:21 +0100

    Remote actors

---
+++ akka-remoting-clustering/src/main/scala/part2_remoting/SimpleActor.scala
@@ -0,0 +1,9 @@
+package part2_remoting
+
+import akka.actor.{Actor, ActorLogging}
+
+class SimpleActor extends Actor with ActorLogging {
+  override def receive: Receive = {
+    case message => log.info(s"Received $message from ${sender()}")
+  }
+}
